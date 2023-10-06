64736bf556ea057013de26a7c0bad44eff1fca82
Alan0777 <camp.alan3@gmail.com>
2023-10-06 02:21:46 -0300

    Teste SCS

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# Copie o arquivo .env.example
+# Crie o arquivo .env a partir do .env.example
 cp .env.example .env
 
-# Iniciar os contêineres Docker
-docker compose up -d
+# Inicie os contêineres Docker
+docker-compose up -d
 
-# Acesse o contêiner PHP e instale as dependências do Composer
-docker compose exec laravel-app bash -c "composer install"
+# Instale as dependências do Composer
+docker-compose exec laravel-app bash -c "composer install"
 
 # Gere a chave de aplicativo Laravel
-docker compose exec laravel-app bash -c "php artisan key:generate"
+docker-compose exec laravel-app bash -c "php artisan key:generate"
 
-# Execute as migrações e seeders (se necessário)
-docker compose exec laravel-app bash -c "php artisan migrate"
-docker compose exec laravel-app bash -c "php artisan db:seed"
+# Execute as migrações do banco de dados
+docker-compose exec laravel-app bash -c "php artisan migrate"
+
+# Execute os seeders do banco de dados (se necessário)
+docker-compose exec laravel-app bash -c "php artisan db:seed"
 
 # Exiba a URL de acesso à aplicação
 echo "Sua aplicação Laravel está disponível em http://localhost:8000"
