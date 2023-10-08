f0477a1097585e126ffa0c7d366070a00787c1c0
Alan0777 <camp.alan3@gmail.com>
2023-10-08 03:23:34 -0300

    Teste SCS

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,7 +2,6 @@
 
 @section('title', isset($task) ? 'Edit Task' : 'Add Task')
 
-
 @section('styles')
 <style>
     .error-message {
@@ -13,45 +12,53 @@
 @endsection
 
 @section('content')
-<form method="POST" action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}">
-    @csrf
-    @isset($task)
-        @method('PUT')
-    @endisset
+<div class="max-w-lg mx-auto">
+    <form method="POST"
+        action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}"
+        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        @csrf
+        @isset($task)
+            @method('PUT')
+        @endisset
 
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}">
-        @error('title')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
+        <div class="mb-4">
+            <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title</label>
+            <input type="text" name="title" id="title"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value="{{ $task->title ?? old('title') }}">
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
 
-    <div>
-        <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5">{{ $task->description ?? old('description') }}</textarea>
-        @error('description')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
+        <div class="mb-4">
+            <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description</label>
+            <textarea name="description" id="description" rows="5"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $task->description ?? old('description') }}</textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
 
-    <div>
-        <label for="long_description">Long description</label>
-        <textarea name="long_description" id="long_description" rows="8">{{ $task->long_description ?? old('long_description') }}</textarea>
-        @error('long_description')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
+        <div class="mb-4">
+            <label for="long_description" class="block text-gray-700 text-sm font-bold mb-2">Long description</label>
+            <textarea name="long_description" id="long_description" rows="8"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $task->long_description ?? old('long_description') }}</textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
 
-    <div>
-        <button class="mb-4 mt-4 font-medium text-gray-700 bord outline outline-offset-2 outline-2 rounded-full bg-gray-200 hover:bg-gray-300"
-        type="submit">
-            @isset($task)
-                Update Task
-            @else
-                Add Task
-            @endisset
-        </button>
-    </div>
-</form>
+        <div class="mb-4">
+            <button type="submit"
+                class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                @isset($task)
+                    Update Task
+                @else
+                    Add Task
+                @endisset
+            </button>
+        </div>
+    </form>
+</div>
 @endsection
