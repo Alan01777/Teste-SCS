488b147c6976935c26bbaedfce616edb34b0881f
Alan0777 <camp.alan3@gmail.com>
2023-10-08 13:32:03 -0300

    Teste SCS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,19 +8,34 @@
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>Task List</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @yield('styles')
 </head>
+
 <body class="container mx-auto mt-10 mb-10 max-w-lg bg-slate-200">
     <h1 class="text-4xl mb-6 text-center">@yield('title')</h1>
 
-    @if (session()->has('status'))
-        <div class=" px-3 py-3 mt-3 mb-3 text-green-800 bg-green-300 rounded-full text-center">
-            {{ session('status') }}
-        </div>
-    @endif
+    <div x-data="{ flash: true }">
+        @if (session()->has('status'))
+          <div>{{ session('status') }}</div>
+          <div x-show="flash"
+            class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+            role="alert">
+            <strong class="font-bold">Success!</strong>
+            <div>{{ session('status') }}</div>
 
-    <div>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke-width="1.5" @click="flash = false"
+                stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </span>
+          </div>
+        @endif
+
         @yield('content')
     </div>
 </body>
+
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,65 +3,69 @@
 @section('title', $task->title)
 
 @section('content')
-<p class="text-lg mb-4">Description: {{ $task->description }}</p>
+    <p class="text-lg mb-4">Description: {{ $task->description }}</p>
 
-@if ($task->long_description)
-    <p class="text-lg mb-4">Details: {{ $task->long_description }}</p>
-@endif
-
-<p class="text-lg mb-4 text-slate-500">Created at: {{ $task->created_at->format('d/m/Y') }}</p>
-
-@if ($task->updated_at)
-    <p class="text-lg mb-4 text-slate-500">Updated at: {{ $task->updated_at->format('d/m/Y') }}</p>
-@endif
-
-<p class="text-lg mb-4">
-    @if ($task->completed == true)
-        Task completed
-    @else
-        Task not completed
+    @if ($task->long_description)
+        <p class="text-lg mb-4">Details: {{ $task->long_description }}</p>
     @endif
-</p>
 
-<div class="flex space-x-4">
-    <div>
-        <form action="{{ route('tasks.index') }}" method="GET">
-            @csrf
-            <button class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            type="submit">
-                Home
-            </button>
-        </form>
-    </div>
+    <p class="text-lg mb-4 text-slate-500">Created {{ $task->created_at->diffForHumans() }}</p>
 
-    <div>
-        <form action="{{ route('tasks.edit', ['task' => $task->id]) }}" method="GET">
-            @csrf
-            <button class="px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50"
-            type="submit">Edit</button>
-        </form>
-    </div>
+    @if ($task->updated_at)
+        <p class="text-lg mb-4 text-slate-500">Updated {{ $task->updated_at->diffForHumans() }}</p>
+    @endif
 
-    <div>
-        <form action="{{ route('tasks.complete', ['task' => $task->id]) }}" method="POST">
-            @csrf
-            @method('PUT')
-            <button class="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-            type="submit">
-                Mark as {{ $task->completed ? 'incompleted' : 'completed' }}
-            </button>
-        </form>
-    </div>
+    <p class="text-lg mb-4">
+        @if ($task->completed == true)
+            <span class="text-green-500 font-medium">Task completed</span>
+        @else
+            <span  class="text-red-500 font-medium">Task not completed</span>
+        @endif
+    </p>
 
-    <div>
-        <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-            type="submit">Delete</button>
-        </form>
+    <div class="flex space-x-4">
+        <div>
+            <form action="{{ route('tasks.index') }}" method="GET">
+                @csrf
+                <button
+                    class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                    type="submit">
+                    Home
+                </button>
+            </form>
+        </div>
+
+        <div>
+            <form action="{{ route('tasks.edit', ['task' => $task->id]) }}" method="GET">
+                @csrf
+                <button
+                    class="px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50"
+                    type="submit">Edit</button>
+            </form>
+        </div>
+
+        <div>
+            <form action="{{ route('tasks.complete', ['task' => $task->id]) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <button
+                    class="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+                    type="submit">
+                    Mark as {{ $task->completed ? 'incompleted' : 'completed' }}
+                </button>
+            </form>
+        </div>
+
+        <div>
+            <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button
+                    class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-full font-semibold focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+                    type="submit">Delete</button>
+            </form>
+        </div>
     </div>
-</div>
 
     </div>
 
